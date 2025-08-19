f18da69b2b148ae624d55cfe38acf7b47597d963
pocamest <pocamest@yandex.ru>
2025-08-19 23:22:34 +0300

    feat(match): add Match database model

---
+++ app/models/match.py
@@ -0,0 +1,33 @@
+import uuid as uuid_pkg
+
+from sqlalchemy import UUID as SQLAlchemyUUID
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.models import Base
+
+
+class Match(Base):
+    __tablename__ = 'Matches'
+
+    id: Mapped[int] = mapped_column('ID', primary_key=True)
+    uuid: Mapped[uuid_pkg.UUID] = mapped_column(
+        'UUID', SQLAlchemyUUID(as_uuid=True), nullable=False, unique=True
+    )
+    player1_id: Mapped[int] = mapped_column(
+        'Player1', ForeignKey('Players.ID'), nullable=False
+    )
+    player2_id: Mapped[int] = mapped_column(
+        'Player2', ForeignKey('Players.ID'), nullable=False
+    )
+    winner_id: Mapped[int | None] = mapped_column(
+        'Winner', ForeignKey('Players.ID'),
+    )
+    score: Mapped[str | None] = mapped_column('Score')
+
+    def __repr__(self) -> str:
+        return (
+            f"<Match(id={self.id!r}, uuid={self.uuid!r}, "
+            f"player1_id={self.player1_id!r}, player2_id={self.player2_id!r}, "
+            f"winner_id={self.winner_id!r})>"
+        )
