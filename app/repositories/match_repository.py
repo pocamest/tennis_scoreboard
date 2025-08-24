3d46312c2f7b6c3a408b9f546470184cda6ee23e
pocamest <pocamest@yandex.ru>
2025-08-24 15:39:32 +0300

    refactor(match_repository): improve method name clarity by including player loading

--- app/repositories/match_repository.py
+++ app/repositories/match_repository.py
@@ -7,7 +7,7 @@ class MatchRepository:
     def __init__(self, session: Session):
         self._session = session
 
-    def get_all_matches(self) -> list[Match]:
+    def find_all_matches_with_players(self) -> list[Match]:
         return (
             self._session.query(Match)
             .options(joinedload(Match.player1), joinedload(Match.player2))
