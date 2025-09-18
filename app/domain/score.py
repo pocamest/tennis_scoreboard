5c2810c6c1978254dbd82d7de82760947e5b8dee
pocamest <pocamest@yandex.ru>
2025-09-18 08:01:21 +0300

    feat(score): implement score calculation logic with tests

--- app/domain/score.py
+++ app/domain/score.py
@@ -1,10 +1,19 @@
-from dataclasses import dataclass
+from __future__ import annotations
+
+from dataclasses import dataclass, replace
 from enum import IntEnum, StrEnum
+from typing import ClassVar, Self, TypeVar
 
 
 class PlayerIdentifier(IntEnum):
     ONE = 0
     TWO = 1
+
+    @property
+    def opponent(self) -> PlayerIdentifier:
+        if self is PlayerIdentifier.ONE:
+            return PlayerIdentifier.TWO
+        return PlayerIdentifier.ONE
 
 
 class PointState(StrEnum):
@@ -15,6 +24,11 @@ class PointState(StrEnum):
     ADVANTAGE = 'AD'
 
 
+# "Компонент Счета" - это атомарная часть счета, т.е. очки, геймы или сеты.
+# Технически, это может быть либо PointState (для очков), либо int (для геймов/сетов).
+TScoreComponent = TypeVar('TScoreComponent', PointState, int)
+
+
 @dataclass(frozen=True)
 class Score:
     """
@@ -22,16 +36,118 @@ class Score:
     Его идентичность определяется совокупностью его атрибутов, а не ID.
     """
 
-    _points: tuple[PointState, PointState] = (PointState.LOVE, PointState.LOVE)
-    _games: tuple[int, int] = (0, 0)
-    _sets: tuple[int, int] = (0, 0)
+    points: tuple[PointState, PointState] = (PointState.LOVE, PointState.LOVE)
+    games: tuple[int, int] = (0, 0)
+    sets: tuple[int, int] = (0, 0)
+
+    _NORMAL_POINT_TRANSITION: ClassVar[dict[PointState, PointState]] = {
+        PointState.LOVE: PointState.FIFTEEN,
+        PointState.FIFTEEN: PointState.THIRTY,
+        PointState.THIRTY: PointState.FORTY,
+    }
+
+    _GAMES_TO_WIN_SET: ClassVar[int] = 6
+    _GAMES_FOR_TIE_BREAK: ClassVar[tuple[int, int]] = (6, 6)
+    _MIN_GAME_DIFFERENCE_FOR_SET_WIN: ClassVar[int] = 2
 
     def get_view_score_model(self) -> dict[str, str | int]:
         return {
-            'player1_points': self._points[PlayerIdentifier.ONE],
-            'player1_games': self._games[PlayerIdentifier.ONE],
-            'player1_sets': self._sets[PlayerIdentifier.ONE],
-            'player2_points': self._points[PlayerIdentifier.TWO],
-            'player2_games': self._games[PlayerIdentifier.TWO],
-            'player2_sets': self._sets[PlayerIdentifier.TWO],
+            'player1_points': self.points[PlayerIdentifier.ONE],
+            'player1_games': self.games[PlayerIdentifier.ONE],
+            'player1_sets': self.sets[PlayerIdentifier.ONE],
+            'player2_points': self.points[PlayerIdentifier.TWO],
+            'player2_games': self.games[PlayerIdentifier.TWO],
+            'player2_sets': self.sets[PlayerIdentifier.TWO],
         }
+
+    def add_point(self, winner: PlayerIdentifier) -> Self:
+        if PointState.ADVANTAGE in self.points or (
+            self.points == (PointState.FORTY, PointState.FORTY)
+        ):
+            return self._handle_deuce_advantage_point(winner)
+        elif PointState.FORTY in self.points:
+            return self._handle_game_point(winner)
+        else:
+            return self._handle_normal_point(winner)
+
+    def _build_score_component_tuple(
+        self,
+        winner: PlayerIdentifier,
+        winner_score_component: TScoreComponent,
+        opponent_score_component: TScoreComponent,
+    ) -> tuple[TScoreComponent, TScoreComponent]:
+        if winner is PlayerIdentifier.ONE:
+            return winner_score_component, opponent_score_component
+        return opponent_score_component, winner_score_component
+
+    def _handle_normal_point(self, winner: PlayerIdentifier) -> Self:
+        opponent = winner.opponent
+        opponent_points = self.points[opponent]
+
+        winner_points = self._NORMAL_POINT_TRANSITION[self.points[winner]]
+
+        new_points = self._build_score_component_tuple(
+            winner=winner,
+            winner_score_component=winner_points,
+            opponent_score_component=opponent_points,
+        )
+        return replace(self, points=new_points)
+
+    def _handle_game_point(self, winner: PlayerIdentifier) -> Self:
+        if self.points[winner] is PointState.FORTY:
+            return self._win_game(winner)
+        return self._handle_normal_point(winner)
+
+    def _handle_deuce_advantage_point(self, winner: PlayerIdentifier) -> Self:
+        if self.points == (PointState.FORTY, PointState.FORTY):
+            opponent = winner.opponent
+            opponent_points = self.points[opponent]
+            winner_points = PointState.ADVANTAGE
+            new_points = self._build_score_component_tuple(
+                winner=winner,
+                winner_score_component=winner_points,
+                opponent_score_component=opponent_points,
+            )
+            return replace(self, points=new_points)
+
+        if self.points[winner] is PointState.ADVANTAGE:
+            return self._win_game(winner)
+
+        return replace(self, points=(PointState.FORTY, PointState.FORTY))
+
+    def _win_game(self, winner: PlayerIdentifier) -> Self:
+        opponent = winner.opponent
+        opponent_games = self.games[opponent]
+        winner_games = self.games[winner] + 1
+        new_games = self._build_score_component_tuple(
+            winner=winner,
+            winner_score_component=winner_games,
+            opponent_score_component=opponent_games,
+        )
+
+        if (
+            winner_games >= self._GAMES_TO_WIN_SET
+            and winner_games - opponent_games >= self._MIN_GAME_DIFFERENCE_FOR_SET_WIN
+        ):
+            return self._win_set(winner)
+
+        if new_games == self._GAMES_FOR_TIE_BREAK:
+            return self._tie_break(winner)
+
+        return replace(self, points=(PointState.LOVE, PointState.LOVE), games=new_games)
+
+    def _win_set(self, winner: PlayerIdentifier) -> Self:
+        opponent = winner.opponent
+        opponent_sets = self.sets[opponent]
+        winner_sets = self.sets[winner] + 1
+        new_sets = self._build_score_component_tuple(
+            winner=winner,
+            winner_score_component=winner_sets,
+            opponent_score_component=opponent_sets,
+        )
+        return replace(
+            self, points=(PointState.LOVE, PointState.LOVE), games=(0, 0), sets=new_sets
+        )
+
+    def _tie_break(self, winner: PlayerIdentifier) -> Self:
+        pass
---
+++ tests/domain/test_score.py
@@ -0,0 +1,165 @@
+import pytest
+
+from app.domain.score import PlayerIdentifier, PointState, Score
+
+NORMAL_POINT_CASES = [
+    pytest.param(
+        Score(),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.FIFTEEN, PointState.LOVE)),
+        id='(Pts:0-0) | P1 wins -> (Pts:15-0)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FIFTEEN, PointState.FORTY)),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.THIRTY, PointState.FORTY)),
+        id='(Pts:15-40) | P1 wins -> (Pts:30-40)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.FIFTEEN)),
+        PlayerIdentifier.TWO,
+        Score(points=(PointState.FORTY, PointState.THIRTY)),
+        id='(Pts:40-15) | P2 wins -> (Pts:40-30)',
+    ),
+]
+
+
+@pytest.mark.parametrize('initial_score, winner, expected_score', NORMAL_POINT_CASES)
+def test_add_point_for_normal_point(
+    initial_score: Score, winner: PlayerIdentifier, expected_score: Score
+) -> None:
+    assert initial_score.add_point(winner) == expected_score
+
+
+GAME_POINT_CASES = [
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.THIRTY)),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.LOVE, PointState.LOVE), games=(1, 0)),
+        id='(Pts:40-30) (Gms:0-0) | P1 wins -> (Pts:0-0) (Gms:1-0)',
+    ),
+    pytest.param(
+        Score(points=(PointState.THIRTY, PointState.FORTY)),
+        PlayerIdentifier.TWO,
+        Score(points=(PointState.LOVE, PointState.LOVE), games=(0, 1)),
+        id='(Pts:30-40) (Gms:0-0) | P2 wins -> (Pts:0-0) (Gms:0-1)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.LOVE), games=(3, 3)),
+        PlayerIdentifier.TWO,
+        Score(points=(PointState.FORTY, PointState.FIFTEEN), games=(3, 3)),
+        id='(Pts:40-0) (Gms:3-3) | P2 wins -> (Pts:40-15) (Gms:3-3)',
+    ),
+    pytest.param(
+        Score(points=(PointState.THIRTY, PointState.FORTY)),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.FORTY, PointState.FORTY), games=(0, 0)),
+        id='(Pts:30-40) (Gms:0-0) | P1 wins -> (Pts:40-40) (Gms:0-0)',
+    ),
+]
+
+
+@pytest.mark.parametrize('initial_score, winner, expected_score', GAME_POINT_CASES)
+def test_add_point_for_game_point(
+    initial_score: Score, winner: PlayerIdentifier, expected_score: Score
+) -> None:
+    assert initial_score.add_point(winner) == expected_score
+
+
+DEUCE_ADVANTAGE_POINT_CASES = [
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.FORTY)),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.ADVANTAGE, PointState.FORTY)),
+        id='(Pts:40-40) | P1 wins -> (Pts:AD-40)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.FORTY)),
+        PlayerIdentifier.TWO,
+        Score(points=(PointState.FORTY, PointState.ADVANTAGE)),
+        id='(Pts:40-40) | P2 wins -> (Pts:40-AD)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.ADVANTAGE)),
+        PlayerIdentifier.ONE,
+        Score(points=(PointState.FORTY, PointState.FORTY)),
+        id='(Pts:40-AD) | P1 wins -> (Pts:40-40)',
+    ),
+    pytest.param(
+        Score(points=(PointState.ADVANTAGE, PointState.FORTY)),
+        PlayerIdentifier.TWO,
+        Score(points=(PointState.FORTY, PointState.FORTY)),
+        id='(Pts:AD-40) | P2 wins -> (Pts:40-40)',
+    ),
+    pytest.param(
+        Score(points=(PointState.ADVANTAGE, PointState.FORTY), games=(1, 1)),
+        PlayerIdentifier.ONE,
+        Score(games=(2, 1)),
+        id='(Pts:AD-40) (Gms:1-1) | P1 wins -> (Pts:00-00) (Gms:2-1)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.ADVANTAGE), games=(1, 1)),
+        PlayerIdentifier.TWO,
+        Score(games=(1, 2)),
+        id='(Pts:40-AD) (Gms:1-1) | P2 wins -> (Pts:00-00) (Gms:1-2)',
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    'initial_score, winner, expected_score', DEUCE_ADVANTAGE_POINT_CASES
+)
+def test_add_point_for_deuce_advantage_point(
+    initial_score: Score, winner: PlayerIdentifier, expected_score: Score
+) -> None:
+    assert initial_score.add_point(winner) == expected_score
+
+
+SET_WIN_CASES = [
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.THIRTY), games=(5, 4)),
+        PlayerIdentifier.ONE,
+        Score(sets=(1, 0)),
+        id='(Pts:40-30) (Gms:5-4) | P1 wins -> (Pts:00-00) (Gms:0-0) (Sts:1-0)',
+    ),
+    pytest.param(
+        Score(points=(PointState.THIRTY, PointState.FORTY), games=(4, 5)),
+        PlayerIdentifier.TWO,
+        Score(sets=(0, 1)),
+        id='(Pts:30-40) (Gms:4-5) | P2 wins -> (Pts:00-00) (Gms:0-0) (Sts:0-1)',
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.THIRTY), games=(6, 5), sets=(0, 1)),
+        PlayerIdentifier.ONE,
+        Score(sets=(1, 1)),
+        id=(
+            '(Pts:40-30) (Gms:6-5) (Sts:0-1) '
+            '| P1 wins -> (Pts:00-00) (Gms:0-0) (Sts:1-1)'
+        ),
+    ),
+    pytest.param(
+        Score(points=(PointState.THIRTY, PointState.FORTY), games=(5, 6), sets=(1, 0)),
+        PlayerIdentifier.TWO,
+        Score(sets=(1, 1)),
+        id=(
+            '(Pts:30-40) (Gms:5-6) (Sts:1-0) '
+            '| P2 wins -> (Pts:00-00) (Gms:0-0) (Sts:1-1)'
+        ),
+    ),
+    pytest.param(
+        Score(points=(PointState.FORTY, PointState.THIRTY), games=(5, 5), sets=(0, 1)),
+        PlayerIdentifier.ONE,
+        Score(games=(6, 5), sets=(0, 1)),
+        id=(
+            '(Pts:40-30) (Gms:5-5) (Sts:0-1) '
+            '| P1 wins -> (Pts:00-00) (Gms:6-5) (Sts:0-1)'
+        ),
+    ),
+]
+
+
+@pytest.mark.parametrize('initial_score, winner, expected_score', SET_WIN_CASES)
+def test_add_point_for_set_win(
+    initial_score: Score, winner: PlayerIdentifier, expected_score: Score
+) -> None:
+    assert initial_score.add_point(winner) == expected_score
